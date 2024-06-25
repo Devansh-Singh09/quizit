102162e59bc4ae0aa409138357f2671137537adb
Devansh Singh <devanshsingh.ds2004@gmail.com>
2024-06-25 23:18:41 +0530

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,14 @@ import { getAuthSession } from '@/lib/nextauth'
 import Link from 'next/link'
 import React from 'react'
 import SignInButton from "./signin btn"
+import UserAccountNav from './UserAccountNav'
+import { ThemeToggle } from './ThemeToggle'
 
 type Props = {}
 
 const navbar = async(props: Props) => {
+    const session = await getAuthSession()
+    
     return(
         <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
@@ -16,7 +20,14 @@ const navbar = async(props: Props) => {
           </p>
         </Link>
         <div className="flex items-center">
-            <SignInButton text="Sign In"/>
+        <ThemeToggle className="mr-4"/>
+        <div className="flex items-center">
+            {session?.user ?(
+                <UserAccountNav user={session.user} />
+            ):(
+                <SignInButton text="Sign In"/>
+            )}
+        </div>
         </div>
       </div>
     </div>
